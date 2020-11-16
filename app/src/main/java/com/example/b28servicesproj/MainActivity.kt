40c91a0d4962c5ad1415547bb64247b432a65df1
Admin <makansari@gmail.com>
2020-11-17 04:17:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/b28servicesproj/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.b28servicesproj
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonStart.setOnClickListener {
+            var i = Intent(this, MyService::class.java)
+            startService(i)
+        }
+
+        buttonStop.setOnClickListener {
+            var i = Intent(this, MyService::class.java)
+            stopService(i)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/b28servicesproj/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.b28servicesproj
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var h = Handler()
+
+        var th = Thread(){
+            kotlin.run {
+                for(i in 1..5){
+                    Thread.sleep(1000)
+                    h.post {
+                        textViewsplash.setText("" + i)
+                    }
+
+                }
+            }
+            var i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
+        th.start()
+    }
+}
